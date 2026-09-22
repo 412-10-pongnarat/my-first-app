88542cbd443aec46f23369aa0bd90b08adbbb9ff
412-10-pongnarat <54280@yupparaj.ac.th>
2026-09-22 15:19:16 +0700

    Update go.py

--- go.py
+++ go.py
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>เกมเติมคำศัพท์อาหารเกาหลี 🇰🇷</title>
+  <title>korean-food-game 🇰🇷</title>
 
   <style>
     * {
